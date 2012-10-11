81fd0d3f5fb97e022ec2f406e68dd9c7ee0124a5
Jihoon <jihoonlee.in@gmail.com>
2012-10-11 14:10:09 +0900

    style adjustment

--- rocon_appmanager_frontend/www/js/appmanager/AppLogView.js
+++ rocon_appmanager_frontend/www/js/appmanager/AppLogView.js
@@ -5,11 +5,12 @@
 
 define(["dojo/_base/declare",
         "dojo/_base/lang",
+        "dojo/dom-style",
         "dijit/_WidgetBase",
         "dijit/form/Button",
-        "dijit/form/Textarea",
+        "dijit/form/SimpleTextarea",
         ],
-function(declare,lang,widgetbase,Button,Textarea)
+function(declare,lang,domStyle,widgetbase,Button,Textarea)
 {
     var AppLogView = declare("appmanager.AppLogView",[widgetbase],
         {
@@ -18,20 +19,34 @@ function(declare,lang,widgetbase,Button,Textarea)
             logs : '',
             
             postCreate : function() {
+                domStyle.set(this.domNode,'width','98%');
+                domStyle.set(this.domNode,'position','relative');
                 this.createTextarea();
                 this.logSubscriber = new ros.Topic({
                     name : this.log_topic_name,
                     type : this.log_topic_type,
                     });
 
-//                this.logSubscriber.subscribe(lang.hitch(this,this.listener));
+                ros.on('connection',lang.hitch(this,this.onConnect)); 
+                ros.on('close',lang.hitch(this,this.onClose));
+            },
+
+            onConnect : function() {
+                this.logSubscriber.subscribe(lang.hitch(this,this.listener));
+            },
+
+            onClose : function() {
+                this.logSubscriber.unsubscribe();
             },
 
             createTextarea : function() {
                 this.textarea = new Textarea({
-                    value : 'No log yet',
+                    value : '',
+                    style : "width:100%; margin-bottom:2pt;",
+                    readOnly : true,
                     });
-                this.domNode.append(this.textarea.domNode);
+                this.domNode.appendChild(this.textarea.domNode);
+                domStyle.set(this.textarea.domNode,'height','200px');
             },
 
             listener : function(msg) {
