cfe7eb3eabebcd6b3476e7d2c436c24a3b12068a
Daniel Stonier <d.stonier@gmail.com>
2012-12-13 21:43:21 +0900

    added hub to launcher, also created pirate launcher for co-testing.

--- concert_master/src/concert_master/concert_master.py
+++ concert_master/src/concert_master/concert_master.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-#       
+#
 # License: BSD
-#   https://raw.github.com/robotics-in-concert/rocon_app_manager/concert_master/LICENSE 
+#   https://raw.github.com/robotics-in-concert/rocon_orchhestration/master/concert_master/LICENSE
 #
 
 import roslib
@@ -18,13 +18,13 @@ from rocon_hub_client.hub_client import HubClient
 class ConcertMaster(object):
     concertmaster_key = "concertmasterlist"
     is_connected = False
-    hub_client = None   
+    hub_client = None
 
     gateway_info = '/gateway/gateway_info'
     gateway = None
 
     set_auto_invite = '/concertconductor/set_auto_invite'
-  
+
     def __init__(self):
         self.is_connected = False
         is_zeroconf = False
@@ -32,24 +32,23 @@ class ConcertMaster(object):
         self.name = rospy.get_name()
         self.param = self.setupRosParameters()
 
-        self.hub_client = HubClient(self.param['hub_whitelist'],self.param['hub_blacklist'],is_zeroconf,'rocon',self.name,False,None)
+        self.hub_client = HubClient(self.param['hub_whitelist'], self.param['hub_blacklist'], is_zeroconf, 'rocon', self.name, False, None)
 
-        self.service= {}
-        self.service['gateway_info'] = rospy.ServiceProxy(self.gateway_info,GatewayInfo)
+        self.service = {}
+        self.service['gateway_info'] = rospy.ServiceProxy(self.gateway_info, GatewayInfo)
         self.service['gateway_info'].wait_for_service()
 
-        self.service['set_auto_invite'] = rospy.ServiceProxy(self.set_auto_invite,SetAutoInvite)
+        self.service['set_auto_invite'] = rospy.ServiceProxy(self.set_auto_invite, SetAutoInvite)
         self.service['set_auto_invite'].wait_for_service()
-
 
     def spin(self):
         self.log("Attempt to connect to Hub...")
         self.connectToHub()
-        self.log("Connected to Hub [%s]"%self.hub_uri)
+        self.log("Connected to Hub [%s]" % self.hub_uri)
         self.log("Registering Concert Master...")
         self.registerConcertMaster()
-        self.log("Concert Master Registered [%s]"%self.name)
-        
+        self.log("Concert Master Registered [%s]" % self.name)
+
         if self.param['auto_invite']:
             self.setAutoInvite()
 
@@ -75,41 +74,38 @@ class ConcertMaster(object):
 
     def setAutoInvite(self):
         try:
-            req = SetAutoInviteRequest(self.name,self.param['auto_invite'])
+            req = SetAutoInviteRequest(self.name, self.param['auto_invite'])
             self.service['set_auto_invite'](req)
         except Exception as e:
             self.logerr("Failed to call [set_auto_invite] : " + str(e))
-            
 
     def registerConcertMaster(self):
         try:
-            self.hub_client.registerKey(self.concertmaster_key,self.name)
+            self.hub_client.registerKey(self.concertmaster_key, self.name)
         except Exception as e:
             self.logerr(str(e))
             traceback.print_exc(file=sys.stdout)
-            
 
     def shutdown(self):
         self.unregisterConcertMaster()
 
     def unregisterConcertMaster(self):
-        self.hub_client.unregisterKey(self.concertmaster_key,self.name)
-
+        self.hub_client.unregisterKey(self.concertmaster_key, self.name)
 
     def setupRosParameters(self):
         param = {}
-        param['hub_uri'] = rospy.get_param('~hub_uri','')
-        param['hub_whitelist'] = rospy.get_param('~hub_whitelist','')
-        param['hub_blacklist'] = rospy.get_param('~hub_blacklist','')
-        param['auto_invite'] = rospy.get_param('~auto_invite',False)
+        param['hub_uri'] = rospy.get_param('~hub_uri', '')
+        param['hub_whitelist'] = rospy.get_param('~hub_whitelist', '')
+        param['hub_blacklist'] = rospy.get_param('~hub_blacklist', '')
+        param['auto_invite'] = rospy.get_param('~auto_invite', False)
 
         return param
 
     def _shutdown(self):
         rospy.loginfo("Shutting down")
 
-    def log(self,msg):
+    def log(self, msg):
         rospy.loginfo("Concert Master : " + msg)
-    def logerr(self,msg):
-        rospy.logerr("Concert Master : " + msg)
 
+    def logerr(self, msg):
+        rospy.logerr("Concert Master : " + msg)
