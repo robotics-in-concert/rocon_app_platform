8bcf965063de4db1f1828aa1635c374221fb2c8a
Daniel Stonier <d.stonier@gmail.com>
2013-01-23 18:22:17 +0900

    catkinized.

---
+++ rocon_appmanager/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['rocon_appmanager'],
+    package_dir={'': 'src'},
+#    scripts=['scripts/gateway_info',
+#             'scripts/remote_gateway_info'
+#             ],
+    requires=['roslib', 'rospy', 'appmanager_msgs', 'concert_client', 'gateway_msgs']
+)
+
+setup(**d)
