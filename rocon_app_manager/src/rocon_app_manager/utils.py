f8db5a6a737289cccbfc273332d5e929ed37817e
Daniel Stonier <d.stonier@gmail.com>
2013-11-07 09:51:43 +0900

    deprecate the old platform info message, #88

--- rocon_app_manager/src/rocon_app_manager/utils.py
+++ rocon_app_manager/src/rocon_app_manager/utils.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 #
 # License: BSD
-#   https://raw.github.com/robotics-in-concert/rocon_app_platform/master/rocon_app_manager/LICENSE
+#   https://raw.github.com/robotics-in-concert/rocon_app_platform/license/LICENSE
 #
 ##############################################################################
 # Imports
 ##############################################################################
 
-import os
 import rospy
 import roslib.names
-import rocon_app_manager_msgs.msg as rapp_manager_msgs
 import rocon_std_msgs.msg as rocon_std_msgs
 from .exceptions import NotFoundException, InvalidPlatformTupleException
 
@@ -20,20 +18,27 @@ from .exceptions import NotFoundException, InvalidPlatformTupleException
 
 
 class PlatformTuple(object):
+    __slots__ = [
+            'os',
+            'version',
+            'system',
+            'platform'
+        ]
 
     def __init__(self, platform_tuple):
         '''
           Converts a platform tuple string into a structure.
-          @param platform_tuple platform.system.robot
+          @param platform_tuple os.version.system.robot
           @type string
         '''
         platform_tuple_list = platform_tuple.split('.')
-        if len(platform_tuple_list) != 3:
-            raise InvalidPlatformTupleException("len('%s') != 3" % platform_tuple)
+        if len(platform_tuple_list) != 4:
+            raise InvalidPlatformTupleException("len('%s') != 4" % platform_tuple)
         # should also validate against rapp_manager_msgs.PLatformInfo constant definitions
-        self.platform = platform_tuple_list[0]
-        self.system = platform_tuple_list[1]
-        self.robot = platform_tuple_list[2]
+        self.os = platform_tuple_list[0]
+        self.version = platform_tuple_list[1]
+        self.system = platform_tuple_list[2]
+        self.platform = platform_tuple_list[3]
 
 
 def find_resource(resource, rospack=None):
@@ -62,11 +67,11 @@ def find_resource(resource, rospack=None):
         raise NotFoundException("multiple resources found [%s]" % (resource))
 
 
-def platform_tuple(platform, system, robot):
+def platform_tuple(os, version, system, platform):
     '''
-      Return the platform tuple string identified by the three strings.
+      Return the platform tuple string identified by the four strings.
     '''
-    return (platform + '.' + system + '.' + robot)
+    return (os + '.' + version + '.' + system + '.' + platform)
 
 
 def platform_compatible(first_platform_tuple, second_platform_tuple):
@@ -74,9 +79,9 @@ def platform_compatible(first_platform_tuple, second_platform_tuple):
       Used to check platform compatibility between app manager
       and its apps.
 
-      @param first_platform_tuple : platform.system.robot
+      @param first_platform_tuple : os.version.system.platform
       @type string
-      @param second_platform_tuple : platform.system.robot
+      @param second_platform_tuple : os.version.system.platform
       @type string
 
       @return false or true depending on compatibility result
@@ -88,14 +93,15 @@ def platform_compatible(first_platform_tuple, second_platform_tuple):
     except InvalidPlatformTupleException as e:
         rospy.logwarn("App Manager : invalid platform tuple [%s]" % str(e))
         return False
-    if platform_one.platform != rapp_manager_msgs.PlatformInfo.PLATFORM_ANY and \
-       platform_two.platform != rapp_manager_msgs.PlatformInfo.PLATFORM_ANY and \
-       platform_one.platform != platform_two.platform:
+    if platform_one.os != rocon_std_msgs.PlatformInfo.OS_ANY and \
+       platform_two.os != rocon_std_msgs.PlatformInfo.OS_ANY and \
+       platform_one.os != platform_two.os:
         return False
+    # Should check version here as well.
     if platform_one.system != platform_two.system:
         return False
-    if platform_one.robot != rapp_manager_msgs.PlatformInfo.ROBOT_ANY and \
-       platform_two.robot != rapp_manager_msgs.PlatformInfo.ROBOT_ANY and \
-       platform_one.robot != platform_two.robot:
+    if platform_one.platform != rocon_std_msgs.PlatformInfo.SYSTEM_ANY and \
+       platform_two.platform != rocon_std_msgs.PlatformInfo.SYSTEM_ANY and \
+       platform_one.platform != platform_two.platform:
         return False
     return True
