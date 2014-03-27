cde3280645e6f5b74037fe7d6aa7bd043ddbfecb
Jihoon Lee <jihoonlee.in@gmail.com>
2014-03-27 09:03:23 +0900

    rename rapp to rocon_app

--- rocon_app_utilities/setup.py
+++ rocon_app_utilities/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['rocon_app_utilities'],
     package_dir={'': 'src'},
-    scripts=['scripts/rapp'],
+    scripts=['scripts/rocon_app'],
     requires=['roslib', 'rospy', 'rocon_python_utils']
 )
 
