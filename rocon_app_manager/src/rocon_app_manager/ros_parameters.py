130eb77d51787a50689bf1816f2d1de8b1bb073b
Daniel Stonier <d.stonier@gmail.com>
2014-07-08 17:36:01 +0900

    remove legacy app store url.

--- rocon_app_manager/src/rocon_app_manager/ros_parameters.py
+++ rocon_app_manager/src/rocon_app_manager/ros_parameters.py
@@ -22,7 +22,6 @@ def setup_ros_parameters():
     param['robot_name'] = rospy.get_param('~robot_name', 'app_manager')  #@IgnorePep8
     # image filename
     param['robot_icon'] = rospy.get_param('~robot_icon', '')  #  #@IgnorePep8
-    param['app_store_url'] = rospy.get_param('~app_store_url', '')  #@IgnorePep8
     param['auto_start_rapp'] = rospy.get_param('~auto_start_rapp', None)  #@IgnorePep8
     param['rapp_package_whitelist'] = rospy.get_param('~rapp_package_whitelist', [])
     param['rapp_package_blacklist'] = rospy.get_param('~rapp_package_blacklist', [])
