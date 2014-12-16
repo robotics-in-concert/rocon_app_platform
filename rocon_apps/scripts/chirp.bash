52a74d359fbe7654e09b77d3c953a51bb1fcf3ae
Jihoon Lee <jihoonlee.in@gmail.com>
2014-12-16 10:31:01 +0900

    volume is now configurable

--- rocon_apps/scripts/chirp.bash
+++ rocon_apps/scripts/chirp.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Set the volume to max
-amixer -c 0 sset PCM,0 100%
-amixer -c 0 sset Master,0 100%
+# Set the volume with second argument
+volume=${2-100}
+
+amixer -c 0 sset PCM,0 $volume%
+amixer -c 0 sset Master,0 $volume%
 
 # Then moo.
 aplay $1
