ce0c65a366a86fcdd4d3221900b7c8a1991fd3a0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-24 00:00:57 +0000

    Adding tarfile member sanitization to extractall()

--- rocon_app_utilities/src/rocon_app_utilities/indexer.py
+++ rocon_app_utilities/src/rocon_app_utilities/indexer.py
@@ -326,7 +326,26 @@ def read_tarball(name=None, fileobj=None, package_whitelist=None, package_blackl
     try:
         logger.debug("read_tarball() unpack to '%s'" % tempdir)
         with tarfile.open(name=name, fileobj=fileobj, mode='r:gz') as tar:
-            tar.extractall(tempdir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, tempdir)
         index = RappIndexer(packages_path=tempdir, package_whitelist=package_whitelist, package_blacklist=package_blacklist)
     finally:
         #shutil.rmtree(tempdir)
